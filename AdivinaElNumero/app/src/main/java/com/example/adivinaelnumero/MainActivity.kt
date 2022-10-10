4d312af1bdf9c3f1234d3b9aefa5ff32826c427a
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-10 19:40:12 +0200

    Inicio del projecto.
    Intento de generar el botón comprobar.

--- AdivinaElNumero/app/src/main/java/com/example/adivinaelnumero/MainActivity.kt
+++ AdivinaElNumero/app/src/main/java/com/example/adivinaelnumero/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.adivinaelnumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var BotonComprobar:Button
+    lateinit var BotonVolverAJugar:Button
+    lateinit var NumeroJugador:EditText
+    lateinit var NumeroAdivinar:Number
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.BotonComprobar=findViewById(R.id.btnComprobar)
+        this.BotonVolverAJugar=findViewById(R.id.btnVolverAJugar)
+        this.NumeroJugador=findViewById(R.id.txtNumber)
+        jugar()
+    }
+
+    private fun jugar() {
+        this.BotonComprobar.isEnabled = true
+        this.BotonVolverAJugar.isEnabled = false
+        this.NumeroAdivinar = (1..100).random()
+    }
+    fun Comprobar(v:View){
+        if (this.NumeroAdivinar.equals(this.NumeroJugador)){
+
+        }
     }
 }
