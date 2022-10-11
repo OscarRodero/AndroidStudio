7646371a5d1f90f7d5c314eca5b14a1cb40813f2
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-11 21:17:45 +0200

    Juego de los Dados funciona (Incluye imágenes que cambian)

--- JuegoDados/app/src/main/java/com/example/juegodados/MainActivity.kt
+++ JuegoDados/app/src/main/java/com/example/juegodados/MainActivity.kt
@@ -2,10 +2,89 @@ package com.example.juegodados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.juegodados.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ImagenDadoJugador.setImageResource(R.drawable.dado1)
+        binding.ImagenDadoPC.setImageResource(R.drawable.dado1)
+
+
+        var contador=0
+        var ptsJugador:Int=0
+        var ptsPC:Int=0
+        ajustarPuntos(ptsJugador, ptsPC)
+        binding.btnVolverAJugar.isEnabled=false
+
+        binding.btnLanzarDados.setOnClickListener{
+            contador=contador+1
+            var x = (1..6).random()
+            cambiarImagenJugador(x)
+            ptsJugador=ptsJugador+x
+            x = (1..6).random()
+            cambiarImagenPC(x)
+            ptsPC=ptsPC+x
+            ajustarPuntos(ptsJugador, ptsPC)
+
+            if(contador==5){
+                contador=0
+                if(Integer.parseInt(binding.PuntosJugador.text.toString())>Integer.parseInt(binding.puntosPC.text.toString())){
+                    val toast = Toast.makeText(applicationContext, "Ganador: Jugador", Toast.LENGTH_LONG)
+                    toast.show()
+                }else if(Integer.parseInt(binding.PuntosJugador.text.toString())<Integer.parseInt(binding.puntosPC.text.toString())){
+                    val toast = Toast.makeText(applicationContext, "Ganador: PC", Toast.LENGTH_LONG)
+                    toast.show()
+                }else{
+                    val toast = Toast.makeText(applicationContext, "Empate", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+                binding.btnVolverAJugar.isEnabled=true
+                binding.btnLanzarDados.isEnabled=false
+            }
+        }
+
+        binding.btnVolverAJugar.setOnClickListener {
+            contador=0
+            ptsJugador=0
+            ptsPC=0
+            ajustarPuntos(ptsJugador, ptsPC)
+            binding.btnLanzarDados.isEnabled=true
+            binding.btnVolverAJugar.isEnabled=false
+        }
+
+    }
+
+    private fun ajustarPuntos(ptsJugador: Int, ptsPC: Int) {
+        binding.PuntosJugador.setText(ptsJugador.toString())
+        binding.puntosPC.setText(ptsPC.toString())
+    }
+
+
+    private fun cambiarImagenPC(x: Int) {
+        when (x){
+            1-> binding.ImagenDadoPC.setImageResource(R.drawable.dado1)
+            2-> binding.ImagenDadoPC.setImageResource(R.drawable.dado2)
+            3-> binding.ImagenDadoPC.setImageResource(R.drawable.dado3)
+            4-> binding.ImagenDadoPC.setImageResource(R.drawable.dado4)
+            5-> binding.ImagenDadoPC.setImageResource(R.drawable.dado5)
+            6-> binding.ImagenDadoPC.setImageResource(R.drawable.dado6)
+        }
+    }
+
+    private fun cambiarImagenJugador(x: Int){
+        when (x){
+            1-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado1)
+            2-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado2)
+            3-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado3)
+            4-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado4)
+            5-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado5)
+            6-> binding.ImagenDadoJugador.setImageResource(R.drawable.dado6)
+        }
     }
 }
