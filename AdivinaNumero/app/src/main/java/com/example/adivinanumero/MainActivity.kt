f48b87693d579816564f66ccebd373886eb82666
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-12 23:10:35 +0200

    Cambios en los botones e intento de implementación de la barra.

--- AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.kt
+++ AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.kt
@@ -11,13 +11,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Variables
         var numero:Int = (1..100).random()
+        var intentos:Int=0
+        //Boton Comprobar
         binding.btnComprobar.setOnClickListener {
             if(binding.etnNumero.equals(numero.toString())){
                 finPartida(true)
+            }else{
+                if(!binding.chkIntentos.isChecked){
+                    intentos++
+                    //binding.BarraIntentos
+                }
+                if(intentos == 5){
+                    finPartida(false)
+                    binding.btnComprobar.isEnabled=false
+                    binding.btnVolverAJugar.isEnabled=true
+                }else{
+                    if(!binding.chkPistas.isChecked){
+                        comprobarNumero(Integer.parseInt(binding.etnNumero.toString()),numero)
+                    }
+                }
             }
         }
+        //Boton Volver a jugar
+        binding.btnVolverAJugar.setOnClickListener {
+            binding.btnComprobar.isEnabled=true
+            binding.btnVolverAJugar.isEnabled=false
+            intentos=0
+            numero=(1..100).random()
+        }
     }
+
+    private fun comprobarNumero(x: Int, numero: Int) {
+        if(numero<x){
+            binding.imageView.setImageResource(R.drawable.flechaabajo)
+        }else{
+            binding.imageView.setImageResource(R.drawable.flechaarriba)
+        }
+    }
+
 
     private fun finPartida(b: Boolean) {
         if(b) {
