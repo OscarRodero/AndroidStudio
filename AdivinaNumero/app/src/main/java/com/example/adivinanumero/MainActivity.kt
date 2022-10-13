9b5ff993501881f0bf2b151c019514d0c52236d7
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-13 20:08:51 +0200

    He perdido el progreso que llevaba, pero he restaurado un punto anterior y he hecho algunos cambios.

--- AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.kt
+++ AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.kt
@@ -11,34 +11,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         //Variables
         var numero:Int = (1..100).random()
+        var numeroJugador:Int=0
         var intentos:Int=0
+
+        //Barra Progreso
+        binding.progressBar.max=5
+        binding.progressBar.progress=0
+
         //Boton Comprobar
         binding.btnComprobar.setOnClickListener {
-            if(binding.etnNumero.equals(numero.toString())){
-                finPartida(true)
-            }else{
-                if(!binding.chkIntentos.isChecked){
-                    intentos++
-                    //binding.BarraIntentos
-                }
-                if(intentos == 5){
-                    finPartida(false)
-                    binding.btnComprobar.isEnabled=false
-                    binding.btnVolverAJugar.isEnabled=true
+            if(!binding.etnNumero.text.isEmpty()){
+                numeroJugador=Integer.parseInt(binding.etnNumero.text.toString())
+                if(numero==numeroJugador){
+                    finPartida(true)
                 }else{
-                    if(!binding.chkPistas.isChecked){
-                        comprobarNumero(Integer.parseInt(binding.etnNumero.toString()),numero)
+                    if(!binding.chkIntentos.isChecked){
+                        intentos++
+                        //binding.BarraIntentos
+                    }
+                    if(intentos == 5){
+                        finPartida(false)
+                        binding.btnComprobar.isEnabled=false
+                        binding.btnVolverAJugar.isEnabled=true
+                    }else{
+                        if(!binding.chkPistas.isChecked){
+                            comprobarNumero(numeroJugador,numero)
+                            binding.progressBar.progress++
+                        }else{
+                            Toast.makeText(this,"Intento gastado", Toast.LENGTH_LONG).show()
+                        }
                     }
                 }
             }
+
         }
         //Boton Volver a jugar
         binding.btnVolverAJugar.setOnClickListener {
             binding.btnComprobar.isEnabled=true
             binding.btnVolverAJugar.isEnabled=false
             intentos=0
+            numeroJugador=0
+            binding.progressBar.progress=0
             numero=(1..100).random()
         }
     }
@@ -54,9 +71,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun finPartida(b: Boolean) {
         if(b) {
-            Toast.makeText(applicationContext, "¡Felicidades, has acertado!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "¡Felicidades, has acertado!", Toast.LENGTH_SHORT).show()
         }else{
-            Toast.makeText(applicationContext,"Has llegado al máximo de intentos. Lo sentimos.",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Has llegado al máximo de intentos. Lo sentimos.",Toast.LENGTH_SHORT).show()
         }
     }
 
