8abcb07a39e35be494a697f071c77d5cb0f8b96e
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-13 21:23:53 +0200

    Agregados botones sin funcionalidad.

--- Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.encuesta
-
+import com.example.encuesta.Resumen
 import android.os.Bundle
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //variables
+        var TextoImprimir:String
+        var Encuestas:ArrayList<Resumen>
         //Switch
         if(binding.switchAnonimo.isActivated){
             binding.PTNombre.isEnabled=false
@@ -20,5 +22,23 @@ class MainActivity : AppCompatActivity() {
             binding.PTNombre.isEnabled=true
         }
         //binding.switchAnonimo.setOnCheckedChangeListener()
+        //Boton Validar
+        binding.btnValidar.setOnClickListener {
+            if(binding.rgSO.checkedRadioButtonId.equals("") && comprobarSistema()==false){
+
+            }
+        }
+        //Boton Resumen
+        binding.btnResumen.setOnClickListener {
+
+        }
+    }
+
+    private fun comprobarSistema():Boolean {
+        var devolver:Boolean=true
+        if (binding.chkASIR.isChecked==false && binding.chkDAM.isChecked==false && binding.chkDAW.isChecked==false){
+            devolver = false
+        }
+        return devolver
     }
 }
---
+++ Encuesta/app/src/main/java/com/example/encuesta/Resumen.kt
@@ -0,0 +1,3 @@
+package com.example.encuesta
+
+data class Resumen(val SistemaOperativo:String, val Especialidad:String,val HorasDeEstudio:Int)
